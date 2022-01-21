1d21e379360f204e29f00492642ecfd3f259d133
mt-igarashi <93467360+mt-igarashi@users.noreply.github.com>
2022-01-21 15:44:42 +0900

    プロセッサー追加

--- src/main/java/com/example/demo/coap/HelloRoute.java
+++ src/main/java/com/example/demo/coap/HelloRoute.java
@@ -1,16 +1,20 @@
 package com.example.demo.coap;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HelloRoute extends RouteBuilder {
 
+    @Autowired
+    private HelloProcessor processor;
+
     @Override
     public void configure() throws Exception {
-        Thread.sleep(500);
+
         from("coap://localhost:5683/hello")
-        .setBody(simple("hello"));
+        .process(processor);
     }
 
 }
